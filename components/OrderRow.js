bfa64f9ea5dcefee534f6695581273f59bd33c1d
Flavio Fraschetti <flavio@riseup.ai>
2021-06-04 12:06:49 +0100

    add OrderRow component

---
+++ components/OrderRow.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, StyleSheet, ViewPropTypes, Text } from 'react-native';
+
+import Colors from '../utils/Colors';
+
+const OrderRow = ({ containerStyle, price, size, total, priceStyle }) => {
+  return (
+    <View>
+      <View style={[[styles.container, containerStyle]]}>
+        <Text style={[styles.text, priceStyle]}>{`${price}`}</Text>
+        <Text style={styles.text}>{`${size}`}</Text>
+        <Text style={styles.text}>{`${total}`}</Text>
+      </View>
+    </View>
+  );
+};
+
+OrderRow.propTypes = {
+  containerStyle: ViewPropTypes.style,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexBasis: 'auto',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  text: {
+    flex: 1,
+    color: Colors.primary,
+    textAlign: 'center',
+  },
+});
+
+export default React.memo(OrderRow);
