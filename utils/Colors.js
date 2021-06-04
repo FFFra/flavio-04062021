c812c8a6b4491e18ede6b55eb8eb1fd1e417ae66
Flavio Fraschetti <flavio@riseup.ai>
2021-06-04 12:05:41 +0100

    create Colors utils

---
+++ utils/Colors.js
@@ -0,0 +1,13 @@
+export default {
+  primary: '#bec3c9',
+  grey: '#485161',
+  dark: '#0f1828',
+  green: '#219467',
+  darkGreen: '#07393a',
+  brightBlue: '#5d3ce2',
+  red: '#d52937',
+  strongRed: '#ca0009',
+  darkRed: '#421e2c',
+};
+
+export const OVERLAY_COLOR = '#b2bbcb';
